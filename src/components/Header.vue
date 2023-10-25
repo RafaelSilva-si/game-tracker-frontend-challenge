5bbeef4080e9f4904270277b69b9de94e5121b09
RafaelSilva-si <rafael.silva@digi.ag>
2023-10-25 06:59:44 -0300

    feat: cria componente de header

---
+++ src/components/Header.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <div class="container">
+      <RouterLink to="/">
+        <img src="../assets/imgs/logo.png" alt="Logo Game Tracker" />
+      </RouterLink>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  background-color: var(--primary-color);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  padding: 0.7rem 0px;
+}
+
+.container {
+  max-width: 960px;
+  margin: 0 auto;
+  & > * {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+
+@media (min-width: 768px) {
+  .container {
+    max-width: 860px;
+    & > * {
+      display: flex;
+      justify-content: start;
+    }
+  }
+}
+
+@media (min-width: 1280px) {
+  .container {
+    max-width: 1080px;
+    & > * {
+      display: flex;
+      justify-content: start;
+    }
+  }
+}
+</style>
