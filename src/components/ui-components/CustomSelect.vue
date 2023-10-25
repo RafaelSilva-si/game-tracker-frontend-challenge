2b0800ce87348619f4de75c215e0d8cb760345a7
RafaelSilva-si <rafael.silva@digi.ag>
2023-10-25 14:18:26 -0300

    feat: cria componente de select customizado

---
+++ src/components/ui-components/CustomSelect.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const { label, handleChange } = defineProps(['label', 'handleChange'])
+</script>
+
+<template>
+  <div class="selectWrapper">
+    <label v-if="label">{{ label }}:</label>
+    <select @change="handleChange">
+      <slot></slot>
+    </select>
+  </div>
+</template>
+
+<style scoped>
+.selectWrapper {
+  text-align: center;
+}
+
+.selectWrapper label {
+  font-size: 16px;
+  line-height: 20px;
+  color: white;
+  width: 100%;
+}
+
+select {
+  background-color: var(--primary-color);
+  border-radius: 8px;
+  border: none;
+  color: #fff;
+  height: 40px;
+  font-weight: 100;
+  padding: 10px;
+  font-size: 12px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  width: 100%;
+}
+
+@media (min-width: 768px) {
+  .selectWrapper {
+    display: flex;
+    align-items: center;
+    width: 100%;
+  }
+}
+</style>
