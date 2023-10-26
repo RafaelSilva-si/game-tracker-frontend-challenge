eac7c791f813011b0006b85c8b812894d07c7ae0
RafaelSilva-si <rafael.silva@digi.ag>
2023-10-25 21:55:15 -0300

    feat: cria tela de pagina não encontrada

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import DealsListPage from '../views/DealsListPage.vue'
 import DealsDetailsPage from '../views/DealsDetailsPage.vue'
 import ErrorPage from '../views/ErrorPage.vue'
+import NotFoundPage from '../views/NotFoundPage.vue'
 
 export default createRouter({
     history: createWebHistory(),
@@ -14,11 +15,16 @@ export default createRouter({
         {
             path: '/deals-details',
             component: DealsDetailsPage,
-            props: (route) => ({ dealsID: route.query.dealsID }),
+            props: (route) => ({ dealsID: route.query.dealId }),
         },
         {
             path: '/error',
             component: ErrorPage,
         },
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'NotFound',
+            component: NotFoundPage
+        }
     ],
 })
---
+++ src/views/NotFoundPage.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <img src="/imgs/page-not-found.png" />
+    <h2>Opss, página não foi encontrada</h2>
+    <p>
+      Desculpe-nos, ocorreu um erro interno no servidor. Nossa equipe técnica já
+      foi notificada e está trabalhando para resolver o problema. Por favor,
+      tente novamente mais tarde. Agradecemos sua compreensão.
+    </p>
+  </main>
+</template>
+
+<style scoped>
+main {
+  max-width: 800px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 4%;
+}
+
+img {
+  width: 300px;
+  height: 300px;
+}
+
+h2 {
+  color: white;
+  font-weight: 500;
+  font-size: 1rem;
+}
+
+p {
+  color: white;
+  text-align: center;
+  font-weight: 300;
+  font-size: 1rem;
+  margin-top: 20px;
+}
+</style>
